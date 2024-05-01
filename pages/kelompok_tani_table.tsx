c960bafbd6f0f8e090a0cd0ad337f5fad854a364
delcode <delcode92@gmail.com>
2024-05-01 08:32:03 +0700

    up

--- pages/kelompok_tani_table.tsx
+++ pages/kelompok_tani_table.tsx
@@ -297,7 +297,7 @@ const Table: React.FC = () => {
 
             {/* MODAL CONTENT */}
             <h1 className={styles.heading}>Edit Petambak</h1>
-            <h2 className={styles.heading2}>{kelompok!='' ? kelompok + ' | Sisa Slot : '+ slot :''}</h2>
+            <h2 className={styles.heading}>{kelompok!='' ? kelompok + ' | Sisa Slot : '+ slot :''}</h2>
             <form onSubmit={handleSubmit}>
             
               {/* KECAMATAN */}
