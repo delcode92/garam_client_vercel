14acc512e43e8074af5d9c0b740d4a929baf44d5
meong92 <38969204+meong92@users.noreply.github.com>
2024-05-02 13:32:31 +0700

    up

--- pages/tampil_env.tsx
+++ pages/tampil_env.tsx
@@ -1,10 +1,11 @@
 const Tampil: React.FC = () => {
     return (
         <ul>
-            <li>{process.env.CLOUDINARY_CLOUD_NAME}</li>
+            <li>test 123</li>
+            {/* <li>{process.env.CLOUDINARY_CLOUD_NAME}</li>
             <li>{process.env.CLOUDINARY_API_KEY}</li>
             <li>{process.env.CLOUDINARY_API_SECRET}</li>
-            <li>{process.env.CLOUDINARY_UPLOAD_PRESET}</li>
+            <li>{process.env.CLOUDINARY_UPLOAD_PRESET}</li> */}
         </ul>
     )
 };
