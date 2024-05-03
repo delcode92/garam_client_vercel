e9621866152ab7ab5f3505cf6305740f814df05e
meong92 <38969204+meong92@users.noreply.github.com>
2024-05-03 15:31:08 +0700

    up

--- pages/peserta_kelompok_tani.tsx
+++ pages/peserta_kelompok_tani.tsx
@@ -281,6 +281,8 @@ const handleLogout = () => {
             <option value="2022">2022</option>
             <option value="2023">2023</option>
             <option value="2024">2024</option>
+            <option value="2025">2025</option>
+            <option value="2026">2026</option>
           </select>
         </div>
 
